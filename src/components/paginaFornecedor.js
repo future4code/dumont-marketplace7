c3632aad5352607da6497187233af84fccb722b1
Renata Caetano <renatapcaetano@hotmail.com>
2020-11-06 12:18:04 -0300

    ignorar

--- src/components/paginaFornecedor.js
+++ src/components/paginaFornecedor.js
@@ -290,7 +290,7 @@ export default class Navbar extends Component {
           Cadastrar produto
         </Button>
 
-        <div>{renderizaTodosProdutos}</div>
+       
 
         <Rodape />
       </div>
